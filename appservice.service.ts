13e5835251ae1d7a10e90cfb3b5e0cb36451270e
MChwalczuk <mateusz.chwalczuk@pollub.edu.pl>
2018-12-09 22:22:12 +0100

    Revert "Merge  Register_F2"

--- appservice.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-
-@Injectable()
-export class AppserviceService {
-
-  constructor() { }
-}
